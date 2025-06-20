9dbc1d8cc5aad4c032c7e92ec14bada0651c6120
Daniela1941 <dlionetti@gmail.com>
2025-06-19 21:06:05 -0300

    Entrego parcial

--- ejercicio19.php
+++ ejercicio19.php
@@ -3,6 +3,13 @@
 // Ejercicio: Escribe una función que reciba un arreglo de números y un número umbral. La función debe devolver un nuevo arreglo solo con los números mayores que el umbral.
 // Complejidad: MEDIA (2 puntos)
 
-function obtenerMayoresQue($numeros, $umbral) {
-    // Tu código aquí
+function obtenerMayoresQue($numeros, $umbral)
+{
+    $numerosmay = [];
+    foreach ($numeros as $numero) {
+        if ($numero > $umbral) {
+            $numerosmay[] = $numero;
+        }
+    }
+    return $numerosmay;
 }
